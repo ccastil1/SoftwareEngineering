7a003f30cc4435d38becd7a36044f38714255f31
Kurt <kdavis23@nd.edu>
2016-11-01 17:07:26 -0400

    #12 Added file deletion functionality

--- se_filesystem/app/controllers/files_controller.rb
+++ se_filesystem/app/controllers/files_controller.rb
@@ -11,13 +11,28 @@ class FilesController < ApplicationController
   end
 
   def remove
-    @file = SeFile.find_by(name: params[:name])
-    @file.delete
-    if SeFile.exists?(@file)
-        output = { :message => "failure" }
-    else
-        output = { :message => "success" }
+    successstr = "success"
+
+    ActiveRecord::Base.transaction do
+      # Remove entry from database    
+      @file = SeFile.find_by(name: params[:name])
+      if @file == nil
+        successstr = "failure"
+      else
+        @file.delete
+      end
+      # Remove file
+      File.delete("/var/lib/se_app/" + params[:name]) if File.exist?("/var/lib/se_app/" + params[:name])
+
+      if File.exist?("/var/lib/se_app" + params[:name])
+        # Raise file exception if delete fails
+        successstr = "failure"
+        raise Errno::ENOENT
+      end
     end
+
+    # Respond with success appropriately
+    output = { :message => successstr }
     render :json => output
   end
 end
