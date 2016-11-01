e98df7ce6cc17dca169e914257453b47b1c5b2eb
Kurt <kdavis23@nd.edu>
2016-11-01 15:57:49 -0400

    #12 Adddatabase delete functionality

--- se_filesystem/app/controllers/files_controller.rb
+++ se_filesystem/app/controllers/files_controller.rb
@@ -8,4 +8,11 @@ class FilesController < ApplicationController
     @file = SeFile.find_by(name: params[:name])
     render json: @file
   end
+
+  def remove
+    @file = SeFile.find_by(name: params[:name])
+    @file.delete
+    output = { :message => "success" }
+    render :json => output
+  end
 end
