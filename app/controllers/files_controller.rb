100e2323e9189fe7ec38e19949e955bcfb7c87c8
Celeste Castillo <ccastil1@nd.edu>
2016-11-08 23:05:40 -0500

    commit

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -45,37 +45,47 @@ class FilesController < ApplicationController
     render :json => output, :status => statusval
   end
 
+def post_params
+  params.require(:se_file).permit(:name, :attachment)
+
+end  
+
+
+
   def upload
-    outputstr = "success: File " + params[:name] + " successfully uploaded!"
+    uploaded_file = SeFile.new post_params
+    file_name = uploaded_file.name
+    uploaded_file.save
+    outputstr = "success: File " + file_name + " successfully uploaded!"
     statusval = 200
 
     #If file already exists in database, update it
-    if SeFile.exists?(name: params[:name]) and params[:data].present?
-      @file = SeFile.find_by(name: params[:name])
-      @file.attachment = params[:data]
-      @file.save
+    # if SeFile.exists?(name: params[:name]) and params[:data].present?
+    #   @file = SeFile.find_by(name: params[:name])
+    #   @file.attachment = params[:data]
+    #   @file.save
 
-      outputstr = "success: File " + params[:name] + " successfully uploaded!"
+    #   outputstr = "success: File " + params[:name] + " successfully uploaded!"
 
-    #If file doesn't exist in database, create it
-    elsif params[:data].present?
-      #Create a new SeFile instance and add it to the database
-      @file = SeFile.new(name: params[:name])
-      @file.attachment = params[:data]
-      @file.save
+    # #If file doesn't exist in database, create it
+    # elsif params[:data].present?
+    #   #Create a new SeFile instance and add it to the database
+    #   @file = SeFile.new(name: params[:name])
+    #   @file.attachment = params[:data]
+    #   @file.save
 
-    end
+    # end
 
     #Raise exception if file is not created
 
-    if not SeFile.exists?(name: params[:name]) or not params[:data].present?
-      #Change output string and raise error
-      outputstr = "failure: File " + params[:name] + "was not uploaded!"
-      statusval = 400
-    end
+  #   if not SeFile.exists?(name: file_name) or not params[:data].present?
+  #     #Change output string and raise error
+  #     outputstr = "failure: File " + file_name + "was not uploaded!"
+  #     statusval = 400
+  #   end
 
-    #output message and status
-    output = { :message => outputstr }
-    render :json => output, :status => statusval
-  end
+     #output message and status
+     output = { :message => outputstr }
+     render :json => output, :status => statusval
+   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,65 @@
 class WelcomeController < ApplicationController
   def index
+        @files = SeFile.all
+        @newFile = SeFile.new
   end
+
+def show
+  @file = SeFile.find(params[:id])
+
+  respond_to do |format|
+    format.html # show.html.erb
+    format.xml  { render :xml => @file }
+  end
+end
+
+def destroy
+    successstr = "failure: File " + params[:name] + " does not exist in server database!" 
+    statusval = 400
+
+    file = SeFile.find_by(name: params[:name])
+    if file.nil?
+      output = { :message => successstr }
+      render :json => output, :status => statusval
+      return
+    end
+
+    # Remove entry from database and delete file
+    file.delete
+
+    successstr = "success: File " + params[:name] + " successfully deleted!"
+    statusval = 200
+
+    # Respond with success appropriately
+    output = { :message => successstr }
+    render :json => output, :status => statusval
+  end
+end
+
+
+def create
+  @file = SeFile.new(params[:name])
+  puts params
+  respond_to do |format|
+    if @file.save
+      flash[:notice] = 'File was successfully uploaded.'
+      format.html { redirect_to(@file) }
+      format.xml  { render :xml => @file, :status => :created,
+	                :location => @file }
+    else
+      format.html { render :action => "new" }
+      format.xml  { render :xml => @file.errors,
+	                :status => :unprocessable_entity }
+    end
+  end
+end
+
+def download
+    file = SeFile.find_by name: params[:name]
+    if file.nil?
+      render json: { message: "File #{params[:name]} not found" }, status: 400
+    else
+      send_file file.attachment.path
+      flash[:notice] = 'File was successfully uploaded.'
+    end
 end
