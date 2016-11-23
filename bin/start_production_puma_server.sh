9d389042a364d5e02a85ac52804405c7ad2d4d84
cshinaver <bshinaver@gmail.com>
2016-11-22 22:19:42 -0500

    issue #36 start production server script takes environment argument

--- bin/start_production_puma_server.sh
+++ bin/start_production_puma_server.sh
@@ -1,8 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-export SECRET_KEY_BASE=$(ruby -e 'require "securerandom"; print SecureRandom.hex(64) + "\n"')
-bundle exec puma -b \
-	'unix:///home/ubuntu/apps/se_filesystem/shared/tmp/sockets/puma.sock' \
-	-e production --control \
-	'unix:///home/ubuntu/apps/se_filesystem/shared/tmp/sockets/pumactl.sock' \
-	-S /home/ubuntu/apps/se_filesystem/shared/tmp/pids/puma.state
+SECRET_KEY_BASE=$(ruby -e 'require "securerandom"; print SecureRandom.hex(64) + "\n"')
+export SECRET_KEY_BASE
+
+usage() {
+    cat <<EOF
+usage: ./start_production_puma_server.sh [-he]
+
+    -e  environment (master, file_node)
+EOF
+}
+
+start_server() {
+    bundle exec puma -b \
+        'unix:///home/ubuntu/apps/se_filesystem/shared/tmp/sockets/puma.sock' \
+        -e "$ENVIRONMENT" --control \
+        'unix:///home/ubuntu/apps/se_filesystem/shared/tmp/sockets/pumactl.sock' \
+        -S /home/ubuntu/apps/se_filesystem/shared/tmp/pids/puma.state
+}
+
+while getopts "he:" arg; do
+    case $arg in
+        h) usage  && exit 0;;
+        e) ENVIRONMENT=$OPTARG ;;
+        *) usage ;;
+    esac
+done
+
+if [[ "$ENVIRONMENT" == "" ]]; then
+    usage
+    exit 1
+else
+    start_server
+fi
