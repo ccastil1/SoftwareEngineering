c29d1c6b2dfed3b46b10cf43ad750aa110b5e15d
Kurt <kdavis23@nd.edu>
2016-12-16 16:48:54 -0500

    Add some class code for upload test

--- test/selenium/site_element.rb
+++ test/selenium/site_element.rb
@@ -2,7 +2,7 @@ require 'selenium-webdriver'
 class SiteElement
 
 	def initialize(url)
-		@driver=Selenium::WebDriver.for :chrome
+		@driver=Selenium::WebDriver.for :firefox
 		@driver.manage.window.maximize
 		@driver.navigate.to url
 	end
@@ -20,7 +20,19 @@ class SiteElement
 	end
 
 	def logout_link()
-		@driver.find_element(:link_text, 'Logout')
+		@driver.find_element(:link_text, "Logout")
+	end
+
+	def upload_name_field
+		@driver.find_element(:id, "upload_name")
+	end
+
+	def upload_file_field
+		@driver.file_element(:id, "upload_file")
+	end
+
+	def upload_button()
+		return @driver.find_element(:id, "upload_submit")
 	end
 
 	def close_browser()
