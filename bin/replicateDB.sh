778c6f6f0f80b02afbc0c5aed5636747a6436f86
spmike <smike@nd.edu>
2016-12-17 20:12:44 -0800

    Changed replication to only work for master.

--- bin/replicateDB.sh
+++ bin/replicateDB.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ $NODE = "Master" ]; then
+        ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.213.130.187 "psql -d inventory_production -c 'DROP TABLE se_files;'"
+        ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.213.130.187 "psql -d inventory_production -c 'DROP TABLE se_files_id_seq;'"
+        ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.213.130.187 "psql -d inventory_production -c 'DROP TABLE file_nodes_id_seq;'"
+        ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.213.130.187 "psql -d inventory_production -c 'DROP TABLE file_nodes_se_files;'"
 	pg_dump -C inventory_production | ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.213.130.187 "psql inventory_production"
 fi
 
-if [ $NODE = "Backup" ]; then
-	pg_dump -C inventory_production | ssh -i ~/.ssh/MasterKeys.pem -C ubuntu@54.218.119.184 "psql inventory_production"
-fi
