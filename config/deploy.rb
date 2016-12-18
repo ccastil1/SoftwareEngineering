bef2c391548199a53d0201d00692d9180369c0c3
spmike <smike@nd.edu>
2016-12-17 23:57:02 -0800

    fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :issue_26
+set :branch,        :issue_26
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
